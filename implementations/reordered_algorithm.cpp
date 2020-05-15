40766e385f4b6543b83f3487bd7b38c0e075aef7
Franz Knobel <knobelf@student.ethz.ch>
2020-05-15 15:23:22 +0200

    reordered algorithm
    integrated backwards step in to K loop in forwards step

--- implementations/reordered_algorithm.cpp
+++ implementations/reordered_algorithm.cpp
@@ -22,12 +22,7 @@
 
 
 static void forward_step_jc(const BWdata& bw, const int& i, const size_t& iter, size_t& res, bool& first, double& neg_log_likelihood_sum_old);
-static void backward_step_jc(const BWdata& bw);
-static void compute_gamma_jc(const BWdata& bw);
-static void compute_sigma_jc(const BWdata& bw);
-static void update_init_prob_jc(const BWdata& bw);
 static void update_trans_prob_jc(const BWdata& bw);
-static void update_emit_prob_jc(const BWdata& bw);
 static size_t comp_bw_scalar_jc1(const BWdata& bw);
 
 
@@ -38,7 +33,7 @@ size_t comp_bw_scalar_jc1(const BWdata& bw){
 
     size_t iter = 0;
     size_t res = 0;
-    double neg_log_likelihood_sum_old; // Does not have to be initialized as it will be if and only if i > 0
+    double neg_log_likelihood_sum_old = 0; // Does not have to be initialized as it will be if and only if i > 0
     bool first = true;
 
     // run for all iterations
@@ -47,30 +42,7 @@ size_t comp_bw_scalar_jc1(const BWdata& bw){
         iter++;
 
         forward_step_jc(bw, i, iter, res, first, neg_log_likelihood_sum_old);
-        backward_step_jc(bw);
-        //compute_gamma_jc(bw);
-        //compute_sigma_jc(bw);
-        //update_init_prob_jc(bw);
         update_trans_prob_jc(bw);
-        //update_emit_prob_jc(bw);
-
-        /*
-        double neg_log_likelihood_sum = 0.0;
-        for (size_t k = 0; k < bw.K; k++) {
-            for (size_t t = 0; t < bw.T; t++) {
-                neg_log_likelihood_sum = neg_log_likelihood_sum + log(bw.c_norm[k*bw.T + t]);
-            }
-        }
-
-        bw.neg_log_likelihoods[i] = neg_log_likelihood_sum;
-
-        if (first && i > 0 && abs(neg_log_likelihood_sum - neg_log_likelihood_sum_old) < 1e-12){
-            first = false;
-            res = iter;
-        }
-
-        neg_log_likelihood_sum_old = neg_log_likelihood_sum;
-        */
     }
 
     return res;
@@ -117,30 +89,12 @@ inline void forward_step_jc(const BWdata& bw, const int& i, const size_t& iter, 
 
             neg_log_likelihood_sum += log(c_norm);
         }
-    }
-
-    // Neg log likelihood sum check
-    bw.neg_log_likelihoods[i] = neg_log_likelihood_sum;
-
-    if (first && i > 0 && abs(neg_log_likelihood_sum - neg_log_likelihood_sum_old) < 1e-12){
-        first = false;
-        res = iter;
-    }
-
-    neg_log_likelihood_sum_old = neg_log_likelihood_sum;
-}
-
-
-inline void backward_step_jc(const BWdata& bw) {
-    for (size_t k = 0; k < bw.K; k++) {
-        // t = bw.T, base case
-        double c_norm = bw.c_norm[k*bw.T + (bw.T-1)];
+        c_norm = bw.c_norm[k*bw.T + (bw.T-1)];
         for (size_t n = 0; n < bw.N; n++) {
             bw.beta[(k*bw.T + (bw.T-1))*bw.N + n] = c_norm;
             bw.ggamma[(k*bw.T + (bw.T-1))*bw.N + n] = bw.alpha[(k*bw.T + (bw.T-1))*bw.N + n];
         }
 
-        // recursion step
         for (int t = bw.T-2; t >= 0; t--) {
             for (size_t n0 = 0; n0 < bw.N; n0++) {
                 double beta_temp = 0.0;
@@ -156,7 +110,6 @@ inline void backward_step_jc(const BWdata& bw) {
 
             }
         }
-
         for (size_t n = 0; n < bw.N; n++) {
             double g_sum = 0.0;
             for (size_t t = 0; t < bw.T-1; t++) {
@@ -174,71 +127,19 @@ inline void backward_step_jc(const BWdata& bw) {
             }
         }
     }
-}
 
+    // Neg log likelihood sum check
+    bw.neg_log_likelihoods[i] = neg_log_likelihood_sum;
 
-inline void compute_gamma_jc(const BWdata& bw) {
-    //for (size_t k = 0; k < bw.K; k++) {
-    //    for (size_t t = 0; t < bw.T; t++) {
-    //        for (size_t n = 0; n < bw.N; n++) {
-    //            bw.ggamma[(k*bw.T + t)*bw.N + n] = bw.alpha[(k*bw.T + t)*bw.N + n] * bw.beta[(k*bw.T + t)*bw.N + n] / bw.c_norm[k*bw.T + t];
-    //        }
-    //    }
-    //}
-
-    // sum up bw.ggamma (from t = 0 to bw.T-2; serve as normalizer for bw.trans_prob)
-    for (size_t k = 0; k < bw.K; k++) {
-        for (size_t n = 0; n < bw.N; n++) {
-            double g_sum = 0.0;
-            for (size_t t = 0; t < bw.T-1; t++) {
-                g_sum += bw.ggamma[(k*bw.T + t)*bw.N + n];
-            }
-            bw.gamma_sum[k*bw.N + n] = g_sum;
-            
-            for (size_t n1 = 0; n1 < bw.N; n1++) {
-                double s_sum = 0.0;
-                for (size_t t = 0; t < bw.T-1; t++) {
-                    s_sum += bw.sigma[((k*bw.T + t)*bw.N + n)*bw.N + n1];
-                }
-                bw.sigma_sum[(k*bw.N + n)*bw.N + n1] = s_sum;
-
-            }
-        }
+    if (first && i > 0 && abs(neg_log_likelihood_sum - neg_log_likelihood_sum_old) < 1e-12){
+        first = false;
+        res = iter;
     }
+
+    neg_log_likelihood_sum_old = neg_log_likelihood_sum;
 }
 
 
-inline void compute_sigma_jc(const BWdata& bw) {
-    for (size_t k = 0; k < bw.K; k++) {
-        //for (size_t t = 0; t < bw.T-1; t++) {
-        //    for (size_t n0 = 0; n0 < bw.N; n0++) {
-        //        for (size_t n1 = 0; n1 < bw.N; n1++) {
-        //            bw.sigma[((k*bw.T + t)*bw.N + n0)*bw.N + n1] = 
-        //                bw.alpha[(k*bw.T + t)*bw.N + n0]*bw.trans_prob[n0*bw.N + n1]*bw.beta[(k*bw.T + (t+1))*bw.N + n1]*bw.emit_prob[n1*bw.M + bw.observations[k*bw.T + (t+1)]];
-        //        }
-        //    }
-        //}
-
-        // sum up bw.sigma (from t = 0 to bw.T-1)
-        for (size_t n0 = 0; n0 < bw.N; n0++) {
-            
-        }
-    }
-}
-
-
-inline void update_init_prob_jc(const BWdata& bw) {
-    for (size_t n = 0; n < bw.N; n++) {
-        
-        for (size_t k = 0; k < bw.K; k++) {
-            
-        }
-        
-    }
-}
-
-// read sigma_sum, gamma_sum, ggamma
-// write trans_prob, init_prob
 inline void update_trans_prob_jc(const BWdata& bw) {
     for (size_t n0 = 0; n0 < bw.N; n0++) {
         for (size_t n1 = 0; n1 < bw.N; n1++) {
@@ -254,8 +155,9 @@ inline void update_trans_prob_jc(const BWdata& bw) {
 
             }
             bw.trans_prob[n0*bw.N + n1] = numerator_sum / denominator_sum;
-            if(n0 == 0)
-                bw.init_prob[n1] = g0_sum/bw.K;
+            if(n0 == 0) {
+                bw.init_prob[n1] = g0_sum / bw.K;
+            }
         }
 
         // Update emit_prob
@@ -281,33 +183,3 @@ inline void update_trans_prob_jc(const BWdata& bw) {
     }
 }
 
-
-inline void update_emit_prob_jc(const BWdata& bw) {
-    /*
-    // add last bw.T-step to bw.gamma_sum
-    for (size_t n0 = 0; n0 < bw.N; n0++) {
-        for (size_t k = 0; k < bw.K; k++) {
-            bw.gamma_sum[k*bw.N + n0] += bw.ggamma[(k * bw.T + (bw.T - 1)) * bw.N + n0];
-        }
-    }
-
-    // update bw.emit_prob
-    for (size_t n0 = 0; n0 < bw.N; n0++) {
-        for (size_t m = 0; m < bw.M; m++) {
-            double numerator_sum = 0.0;
-            double denominator_sum = 0.0;
-            for (size_t k = 0; k < bw.K; k++) {
-                double ggamma_cond_sum = 0.0;
-                for (size_t t = 0; t < bw.T; t++) {
-                    if (bw.observations[k*bw.T + t] == m) {
-                        ggamma_cond_sum += bw.ggamma[(k*bw.T + t)*bw.N + n0];
-                    }
-                }
-                numerator_sum += ggamma_cond_sum;
-                denominator_sum += bw.gamma_sum[k*bw.N + n0];
-            }
-            bw.emit_prob[n0 * bw.M + m] = numerator_sum / denominator_sum;
-        }
-    }
-     */
-}
